9c4d68192c323104039479cb36c69994d12f79f7
mjakus <margaret.jakus@students.olin.edu>
2017-11-28 18:47:40 -0500

    working on add round key

--- AddRoundKey.v
+++ AddRoundKey.v
@@ -1,23 +1,46 @@
 module AddRoundKey(
 
-input [4:4] RoundKey; 
-input [4:4] State;
-output [4:4] ARKOut;
+input [1:0][3:0] RoundKey,
+input [1:0][3:0] State,
+output [1:0][3:0] ARKOut
 
 );
 
 
 // each byte (entry) of the state is combined with a block of the round key using bitwise xor
-
-	for (column = 0; column < 4; column = column + 1) begin
+	genvar i, j; 
+	generate 
+	
+	for (i = 0; i < 4; i = i + 1) begin
 		
-		for (row = 0; row < 4; row = row + 1) begin
+		for (j = 0; j < 2; j = j + 1) begin
 			
-			xor(ARKOut(row, column), RoundKey(row, column), State(row, column)); // this almost definitely won't work
+			xor(ARKOut[j][i], RoundKey[j][i], State[j][i]); // this almost definitely won't work
 			
 		end
 		
 	end
+	endgenerate
 
 endmodule
 
+
+module testARK();
+
+
+reg [1:0][3:0] rk;
+reg [1:0][3:0] s;
+wire [1:0][3:0] ao;
+
+AddRoundKey test1(rk, s, ao);
+
+initial begin
+
+$display("AOut | RK | S");
+rk = 4'b0101; s = 4'b0100; #20
+$display("%b | %b | %b", ao, rk, s);
+
+
+end
+
+endmodule
