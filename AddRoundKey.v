ad6a0356b1f01b7ff2b6f2bcdbdcc2a1791ac519
mjakus <margaret.jakus@students.olin.edu>
2017-11-28 20:36:12 -0500

    AddRoundKey seems to work just fine. Having problems assigning values in ShiftRows. Indexing seems to be wrong

--- AddRoundKey.v
+++ AddRoundKey.v
@@ -1,43 +1,39 @@
 module AddRoundKey(
 
-input [1:0][3:0] RoundKey,
-input [1:0][3:0] State,
-output [1:0][3:0] ARKOut
+input [1:0][1:0][3:0] RoundKey,
+input [1:0][1:0][3:0] State,
+output [1:0][1:0][3:0] ARKOut
 
 );
-
-
 // each byte (entry) of the state is combined with a block of the round key using bitwise xor
-	genvar i, j; 
+	genvar i, j, c; 
 	generate 
 	
-	for (i = 0; i < 4; i = i + 1) begin
+	for (c = 0; c < 2; c = c + 1) begin
+		for (i = 0; i < 4; i = i + 1) begin	
+			for (j = 0; j < 2; j = j + 1) begin		
+				xor(ARKOut[c][j][i], RoundKey[c][j][i], State[c][j][i]); // this almost definitely won't work
+			
+			end
 		
-		for (j = 0; j < 2; j = j + 1) begin
-			
-			xor(ARKOut[j][i], RoundKey[j][i], State[j][i]); // this almost definitely won't work
-			
 		end
-		
 	end
 	endgenerate
 
 endmodule
 
-
 module testARK();
 
-
-reg [1:0][3:0] rk;
-reg [1:0][3:0] s;
-wire [1:0][3:0] ao;
+reg [1:0][1:0][3:0] rk;
+reg [1:0][1:0][3:0] s;
+wire [1:0][1:0][3:0] ao;
 
 AddRoundKey test1(rk, s, ao);
 
 initial begin
 
 $display("AOut | RK | S");
-rk = 4'b0101; s = 4'b0100; #20
+rk = 16'b0011001100110001; s = 16'b0011001000110010; #20
 $display("%b | %b | %b", ao, rk, s);
 
 
--- ShiftRows.v
+++ ShiftRows.v
@@ -1,7 +1,54 @@
 module ShiftRows(
 
+input [1:0][1:0][3:0] State,
+output [1:0][1:0][3:0] SROut,
+output[1:0][1:0][3:0]X,
+output [1:0][1:0][3:0]Y
 );
 
 
 
+assign SROut = State;
+
+	genvar j; 
+	generate
+		for (j = 0; j < 2; j = j + 1) begin
+			assign X[1][j][3] = State[1][j][3]; // clearly something is going wrong here. Nothing is being assigned.
+			assign Y[0][j][3] = State[0][j][3]; 
+		end
+	endgenerate
+	
+	/*
+	always @(*) begin
+		if (j < 2) begin
+			X[j] <= State[1][j][3];
+			Y[j] <= State[0][j][3];
+			j <= j + 1;
+		end
+	end*/
+
 endmodule
+
+module testSR();
+
+reg [1:0][1:0][3:0] s;
+wire [1:0][1:0][3:0] so;
+wire[1:0][1:0][3:0]X;
+wire [1:0][1:0][3:0]Y;
+
+ShiftRows shift(s, so,X,Y);
+
+initial begin
+
+$display("ShiftOut | StateIn | X | Y");
+s = 16'b0000001100000000;
+$display("%b | %b ", so, s); // clearly our indexing is messed up, but we should be able to get these values into X and Y
+$display("%b", X[1][0][3]); // X[0]
+$display("%b", X[1][1][3]); // X[1]
+$display("%b", Y[0][0][3]); // Y[0]
+$display("%b", Y[0][1][3]); // Y[1]
+end
+
+endmodule
+
+
