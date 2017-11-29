f912a940f42cd6bcbe9d9dcd0f102bafac1d0b99
mjakus <margaret.jakus@students.olin.edu>
2017-11-29 18:17:09 -0500

    making progress but having ShiftRows-related problems

--- AddRoundKey.v
+++ AddRoundKey.v
@@ -1,40 +1,43 @@
+// don't actually need this for ARK. Will need for Shift Rows.
+`define STATE(r,c) inarray[(elements*(c-1))+(r-1)];
+`define ROUNDKEY(r,c) keyarray[(elements*(c-1))+(r-1)];
+`define OUT(r,c) outarray[(elements*(c-1))+(r-1)];
+
 module AddRoundKey(
-
-input [1:0][1:0][3:0] RoundKey,
-input [1:0][1:0][3:0] State,
-output [1:0][1:0][3:0] ARKOut
-
-);
 // each byte (entry) of the state is combined with a block of the round key using bitwise xor
-	genvar i, j, c; 
-	generate 
-	
-	for (c = 0; c < 2; c = c + 1) begin
-		for (i = 0; i < 4; i = i + 1) begin	
-			for (j = 0; j < 2; j = j + 1) begin		
-				xor(ARKOut[c][j][i], RoundKey[c][j][i], State[c][j][i]); // this almost definitely won't work
-			
-			end
-		
+input [3:0][1:0] inarray,
+input [3:0][1:0] keyarray,
+output [3:0][1:0] outarray
+);
+
+parameter elements = 2;
+
+genvar i, j;
+generate
+	for (i = 0; i < 4; i = i+1) begin
+		for (j = 0; j < 2; j = j + 1) begin		
+			xor(outarray[i][j], inarray[i][j], keyarray[i][j]);
 		end
 	end
-	endgenerate
+endgenerate
+
 
 endmodule
 
 module testARK();
 
-reg [1:0][1:0][3:0] rk;
-reg [1:0][1:0][3:0] s;
-wire [1:0][1:0][3:0] ao;
+reg [3:0][1:0] rk;
+reg [3:0][1:0] s;
+wire [3:0][1:0] ao;
 
 AddRoundKey test1(rk, s, ao);
 
 initial begin
 
 $display("AOut | RK | S");
-rk = 16'b0011001100110001; s = 16'b0011001000110010; #20
+rk = 8'b00011111; s = 8'b11111011; #20
 $display("%b | %b | %b", ao, rk, s);
+
 
 
 end
--- ShiftRows.v
+++ ShiftRows.v
@@ -1,15 +1,34 @@
+`define STATE(r,c) inarray[(elements*(c-1))+(r-1)];
+`define OUT(r,c) outarray[(elements*(c-1))+(r-1)];
+
+
 module ShiftRows(
 
-input [1:0][1:0][3:0] State,
-output [1:0][1:0][3:0] SROut,
-output[1:0][1:0][3:0]X,
-output [1:0][1:0][3:0]Y
+input [3:0] inarray,
+input clk,
+output [3:0] outarray,
+output X,
+output Y
 );
+parameter elements = 2;
+
+reg [1:0] counter = 2'b0;
+
+always @(posedge clk) begin
+	counter = counter + 1;
+	
+	if (counter == 1) begin
+		X <= `STATE(2,1);
+		Y <= `STATE(2,2);
+	end
+	/*if (counter == 2) begin
+		`STATE(2,1) <= Y;
+	end*/
 
 
+end
 
-assign SROut = State;
-
+/*
 	genvar j; 
 	generate
 		for (j = 0; j < 2; j = j + 1) begin
@@ -17,7 +36,7 @@ assign SROut = State;
 			assign Y[0][j][3] = State[0][j][3]; 
 		end
 	endgenerate
-	
+	*/
 	/*
 	always @(*) begin
 		if (j < 2) begin
@@ -31,22 +50,22 @@ endmodule
 
 module testSR();
 
-reg [1:0][1:0][3:0] s;
-wire [1:0][1:0][3:0] so;
-wire[1:0][1:0][3:0]X;
-wire [1:0][1:0][3:0]Y;
+reg [3:0] s;
+reg clk;
+wire [3:0] so;
+wire X;
+wire Y;
 
-ShiftRows shift(s, so,X,Y);
+ShiftRows shift(s, clk, so, X, Y);
 
 initial begin
 
 $display("ShiftOut | StateIn | X | Y");
-s = 16'b0000001100000000;
+s = 4'b0101;
 $display("%b | %b ", so, s); // clearly our indexing is messed up, but we should be able to get these values into X and Y
-$display("%b", X[1][0][3]); // X[0]
-$display("%b", X[1][1][3]); // X[1]
-$display("%b", Y[0][0][3]); // Y[0]
-$display("%b", Y[0][1][3]); // Y[1]
+$display("%b", X); // X[0]
+$display("%b", Y); // X[1]
+
 end
 
 endmodule
