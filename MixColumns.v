3b6244c7b95ceee7dcae8fd4ca458d799d27e035
mjakus <margaret.jakus@students.olin.edu>
2017-11-30 17:57:30 -0500

    ShiftRows with 4x4 matrix (each entry only 1 bit tho) working!

--- MixColumns.v
+++ MixColumns.v
@@ -1,5 +1,9 @@
 // unclear if we need macros here either, since we're not actually moving things around.
 
+`define STATE(r,c) inarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
+
+`define OUT(r,c) outarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
+
 module MixColumns(
 input [7:0] inarray, // [7:0] is a two column, four row matrix
 input clk,
@@ -7,13 +11,21 @@ output reg [7:0] outarray
 );
 
 
-// for each column 
-// if row = 1, then do x2
+always @(posedge clk) begin
+// THIS STUFF BELOW IS FALSE
+// if row = 1, then do x2 (shift left 1, then xor with 1b)
 // if row = 2, then do 3x
 // if row = 3, then do x
 // if row = 4, then do x
+	column = column + 1;
+	if (column == 1) begin
+		if (row == 1) begin
+			X(row, column) <= `STATE(
+			`OUT(row, column) <= `
+		end
+	end
 
-
+end
 
 
 
--- ShiftRows.v
+++ ShiftRows.v
@@ -4,12 +4,12 @@
 
 module ShiftRows (
 
-input [3:0] inarray,
+input [15:0] inarray,
 input clk,
-output reg [3:0] outarray
+output reg [15:0] outarray
 
 );
-parameter dimension = 2;
+parameter dimension = 4;
 reg X;
 reg Y;
 reg [1:0] counter = 2'b0;
@@ -18,15 +18,30 @@ always @(posedge clk) begin
 	counter = counter + 1;
 	
 	if (counter == 1) begin
-		`OUT(2,2) <= `STATE(2,1) ;
+		`OUT(1,1) <= `STATE(1,1) ;
+		`OUT(1,2) <= `STATE(1,2) ;
+		`OUT(1,3) <= `STATE(1,3) ;
+		`OUT(1,4) <= `STATE(1,4) ;
+	
+		`OUT(2,4) <= `STATE(2,1) ;
 		`OUT(2,1) <= `STATE(2,2) ;
+		`OUT(2,2) <= `STATE(2,3) ;
+		`OUT(2,3) <= `STATE(2,4) ;
+		
+		`OUT(3,3) <= `STATE(3,1) ;
+		`OUT(3,4) <= `STATE(3,2) ;
+		`OUT(3,1) <= `STATE(3,3) ;
+		`OUT(3,2) <= `STATE(3,4) ;
+		
+		`OUT(4,2) <= `STATE(4,1) ;
+		`OUT(4,3) <= `STATE(4,2) ;
+		`OUT(4,4) <= `STATE(4,3) ;
+		`OUT(4,1) <= `STATE(4,4) ;
+		
 	end
-	/*
-	if (counter == 2) begin
-		//`OUT(2,1) <= Y;
-		outarray[3] <= Y;
+	else if (counter == 2) begin
+		counter <= 0;
 	end
-*/
 
 end
 
@@ -34,9 +49,9 @@ endmodule
 
 module testSR();
 
-reg [3:0] s;
+reg [15:0] s;
 reg clk;
-wire [3:0] so;
+wire [15:0] so;
 ShiftRows shift(s, clk, so);
 
     initial clk=0;
@@ -45,11 +60,8 @@ ShiftRows shift(s, clk, so);
 initial begin
 
 $display("ShiftOut | StateIn | X | Y");
-s = 4'b1011; #200
-$display("%b | %b ", so, s); // clearly our indexing is messed up, but we should be able to get these values into X and Y
-//$display("%b", X); // X[0]
-//$display("%b", Y); // X[1]
-
+s = 16'b1011001000001111; #200
+$display("%b | %b ", so, s); // clearly our indexing is messed up, but we should be able to get these values into 
 $finish;
 end
 
