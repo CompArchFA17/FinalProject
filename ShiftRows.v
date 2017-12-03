855deeb46edee897d627e91cb8170b42710a3411
mjakus <margaret.jakus@students.olin.edu>
2017-12-03 07:31:11 -0500

    shift rows is fully functional!

--- ShiftRows.v
+++ ShiftRows.v
@@ -1,12 +1,15 @@
-`define STATE(r,c) inarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
+`define STATE2(r,c) inarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
 
-`define OUT(r,c) outarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
+`define OUT2(r,c) outarray[(dimension*dimension-1)-((dimension*(c-1))+(r-1))]
+
+`define STATE(r, c) inarray[((4*c)+(8*r)+7):((4*c)+(8*r))]
+`define OUT(r, c) outarray[((4*c)+(8*r)+7):((4*c)+(8*r))]
 
 module ShiftRows (
 
-input [15:0] inarray,
+input [127:0] inarray,
 input clk,
-output reg [15:0] outarray
+output reg [127:0] outarray
 
 );
 parameter dimension = 4;
@@ -18,27 +21,28 @@ always @(posedge clk) begin
 	counter = counter + 1;
 	
 	if (counter == 1) begin
-	/*
-		`OUT(1,1) <= `STATE(1,1) ;
-		`OUT(1,2) <= `STATE(1,2) ;
-		`OUT(1,3) <= `STATE(1,3) ;
-		`OUT(1,4) <= `STATE(1,4) ;
 	
-		`OUT(2,4) <= `STATE(2,1) ;
-		`OUT(2,1) <= `STATE(2,2) ;
-		`OUT(2,2) <= `STATE(2,3) ;
-		`OUT(2,3) <= `STATE(2,4) ;
+		`OUT(3, 0) <= `STATE(3, 0) ;
+		`OUT(3, 8) <= `STATE(3, 8) ;
+		`OUT(3, 16) <= `STATE(3, 16) ;
+		`OUT(3, 24) <= `STATE(3, 24) ;
 		
-		`OUT(3,3) <= `STATE(3,1) ;
-		`OUT(3,4) <= `STATE(3,2) ;
-		`OUT(3,1) <= `STATE(3,3) ;
-		`OUT(3,2) <= `STATE(3,4) ;
+		`OUT(2, 8) <= `STATE(2, 0) ;
+		`OUT(2, 16) <= `STATE(2, 8) ;
+		`OUT(2, 24) <= `STATE(2, 16) ;
+		`OUT(2, 0) <= `STATE(2, 24) ;
 		
-		`OUT(4,2) <= `STATE(4,1) ;
-		`OUT(4,3) <= `STATE(4,2) ;
-		`OUT(4,4) <= `STATE(4,3) ;
-		`OUT(4,1) <= `STATE(4,4) ; */
+		`OUT(1, 16) <= `STATE(1, 0) ;
+		`OUT(1, 24) <= `STATE(1, 8) ;
+		`OUT(1, 0) <= `STATE(1, 16) ;
+		`OUT(1, 8) <= `STATE(1, 24) ;		
 		
+		
+		`OUT(0, 24) <= `STATE(0, 0) ;
+		`OUT(0, 0) <= `STATE(0, 8) ;
+		`OUT(0, 8) <= `STATE(0, 16) ;
+		`OUT(0, 16) <= `STATE(0, 24) ;		
+				
 	end
 	else if (counter == 2) begin
 		counter <= 0;
@@ -50,9 +54,9 @@ endmodule
 
 module testSR();
 
-reg [15:0] s;
+reg [127:0] s;
 reg clk;
-wire [15:0] so;
+wire [127:0] so;
 ShiftRows shift(s, clk, so);
 
     initial clk=0;
@@ -61,7 +65,7 @@ ShiftRows shift(s, clk, so);
 initial begin
 
 $display("ShiftOut | StateIn | X | Y");
-s = 16'b1011001000001111; #200
+s = 32'b1011001000001111; #200
 $display("%b | %b ", so, s); // clearly our indexing is messed up, but we should be able to get these values into 
 $finish;
 end
