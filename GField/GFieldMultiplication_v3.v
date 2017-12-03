1efa4ae45bb82b440a968ae2174e85e990a28ba9
LoganSweet <logan.sweet@students.olin.edu>
2017-12-03 16:53:41 -0500

    wrong answers but no errors in v3

--- GField/GFieldMultiplication_v3.v
+++ GField/GFieldMultiplication_v3.v
@@ -2,48 +2,54 @@
 
 module GFieldMultiplyFourv3(
 input clk, 
-input [0:3] a, 
-input [0:3] b, 
-output [0:3] prod4
+input [0:7] a, 
+input [0:7] b, 
+output [0:7] prod4
 );
-reg [0:3] product = 0000; 
-wire [0:3] p; 
-//reg counter = 0; 
-reg [0:3] counter;
-reg [0:3] c = 1011;
-reg [0:3] a2 ; 
-reg [0:3] b2 ; 
+reg [0:7] product = 8'b00000000; 
+wire [0:7] p; 
+reg [0:7] counter;
+reg [0:7] c = 8'b00011011;
+reg [0:7] a2 ; 
+reg [0:7] b2 ; 
 
-initial a2 = a; 
-initial b2 = b; 
+//initial a2[0:3] <= a[0:3];
+//initial b2[0:3] <= b[0:3];
+//initial a2 = a; 
+//initial b2 = b; 
 initial counter = 0; 
 
 always @(posedge clk) begin 
 
-if (counter < 4)begin 
+if (counter == 0) begin
+	assign a2[0:7] = a[0:7];
+	assign b2[0:7] = b[0:7];
+	counter = counter+1; 
+end 	
+
+if (counter < 9)begin 
 
 	if(b[0] == 1) begin
-		product <= product ^ a ;
-		//prod4 <= prod4 ^ a ;
+		//product <= product ^ a ;
+		assign product = product ^ a2 ;
 	end  // if b end 
 	
-	if(a2[3] == 1) begin
-		a2[0:3] = {a2[0:2], 1'b0} ;
-		a2 = a2 ^ c ;
+	if(a2[7] == 1) begin
+		assign a2[0:7] = {a2[0:6], 1'b0} ;
+		assign a2 = a2 ^ c ;
 	end  
-	if (a2[3] == 0)
-		a2[0:3] = {a2[0:2], 1'b0} ;			
-	b2[0:3] = {1'b0 , b2[0:2]} ;	
+	
+	if (a2[7] == 0)
+		assign a2[0:7] = {a2[0:6], 1'b0} ;		
+			
+	assign b2[0:7] = {1'b0 , b2[0:6]} ;	
+
+	$display(counter, "  ", a, " ",b,  "  ", a2, " ", b2, "  ", product);
+
 
 counter = counter+1 ;
+//product <= prod4;
 
-product <= prod4;
- 
-$display(counter);
-$display("product");
-
-$display(product);
-	
 end   //if counter end
 
 end   // always end 
@@ -53,9 +59,9 @@ endmodule
 
 module GFieldMultiplyFour_test();
 reg clock; 
-reg [0:3]x ; 
-reg [0:3]y;
-wire [0:3]p; 
+reg [0:7]x ; 
+reg [0:7]y;
+wire [0:7]p; 
 
 //GFieldMultiplyFour test10(clock, x, y, p);
 GFieldMultiplyFourv3 potato(clock, x, y, p);
@@ -64,13 +70,13 @@ GFieldMultiplyFourv3 potato(clock, x, y, p);
     always #10 clock=!clock;    // 50MHz Clock  
 
 initial begin
-$display("  x  |  y   |  p  " );
-x = 4'b0000; y = 4'b0000; #100
-$display("%b | %b | %b ", x, y, p);
-x = 4'b0001; y = 4'b0000; #100
-$display("%b | %b | %b ", x, y, p);
-x = 4'b1111; y = 4'b0010; #100
-$display("%b | %b | %b ", x, y, p);
+//$display("  x  |  y   |  p  " );
+x = 8'b00000111; y = 8'b0000011; #100
+//$display("%b | %b | %b ", x, y, p);
+//x = 4'b0001; y = 4'b0000; #100
+//$display("%b | %b | %b ", x, y, p);
+//x = 4'b1111; y = 4'b0010; #100
+//$display("%b | %b | %b ", x, y, p);
 
 
 
