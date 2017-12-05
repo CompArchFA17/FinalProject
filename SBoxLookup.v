dd5d555239d8b52b800cf4cdd8817e392b5169c0
mjakus <margaret.jakus@students.olin.edu>
2017-12-05 11:07:28 -0500

    SubBytes fully functional

--- SBoxLookup.v
+++ SBoxLookup.v
@@ -292,44 +292,7 @@ case(in)
 endcase
 
 endmodule 
-
-module lut(count_out, angle);
-input [2:0] count_out;
-output [11:0] angle;
-reg [11:0] angle;
-
-always @(count_out)
-case (count_out)
-3'b000: angle=12'b001000000000;	//0	45	45
-3'b001: angle=12'b000100101110;	//1	26.54	26.57
-3'b010: angle=12'b000010100000;	//2	14.06	14.036
-3'b011: angle=12'b000001010001;	//3	7.12	7.13
-3'b100: angle=12'b000000101001;	//4	3.604	3.576
-3'b101: angle=12'b000000010100;	//5	1.76	1.79
-3'b110: angle=12'b000000001010;	//6	0.88	0.9
-3'b111: angle=12'b000000000101;	//7	0.44	0.45
-default: angle=12'b001000000000; //default 0
-endcase
-
-endmodule
-
-module testlut();
-reg [2:0] co;
-wire [11:0] a;
-
-lut look(co, a);
-
-initial begin
-co = 3'b010; #20
-$display("%b | %b ", co, a);
-
-co = 3'b111; #20
-$display("%b | %b ", co, a);
-end
-
-endmodule
-
-
+/*
 module testSBox();
 reg [7:0] innum;
 wire [7:0] outnum;
@@ -359,4 +322,4 @@ $display("%b", outnum);
 
 end
 
-endmodule
+endmodule*/
--- SubBytes.v
+++ SubBytes.v
@@ -1,18 +1,32 @@
 `include "SBoxLookup.v" 
 
 module SubBytes(
-reg [127:0] ARKOut,
-wire [127:0] SBOut
-
+input [127:0] ARKOut,
+output [127:0] SBOut
 );
+
 genvar i;
 generate
 for (i = 1; i < 17; i = i + 1) begin
-	SBoxLookup assignvalues(ARKOut[i, SBOut);
-	
+	SBoxLookup assignvalues(ARKOut[((8*i)-1):(8*(i-1))], SBOut[((8*i)-1):(8*(i-1))]);
 end
 
 endgenerate
 
 endmodule
 
+module testSubBytes();
+
+reg [127:0] AO;
+wire [127:0] SBO;
+
+SubBytes testSB(AO, SBO);
+		
+initial begin
+$display("Output | Expect | Input ");
+AO = 127'b0; #40
+$display("%b | 01100011 | %b ", SBO[7:0], AO[7:0]);
+end
+
+
+endmodule
