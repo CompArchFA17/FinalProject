3080450b2a61dad78c9550ee2f7dfa99afd6e33c
arianaolson419 <arianaolson419@gmail.com>
2017-12-06 20:13:57 -0500

    Parameterize and add header comments

--- arithmetic.v
+++ arithmetic.v
@@ -1,7 +1,22 @@
-
-module multiplication (
-	input [4:0] a, b,
-	output [4:0] result
+"""
+Performs a scalar multiplication on two numbers.
+Represents the operation a * b = c.
+Inputs: 
+	- a and b are the two scalars being multiplied
+Outputs: 
+	- result is the scalar product.
+Parameters:
+	- ENTRY_SIZE is the size of a and b
+	- RESENTRY_SIZE is the size of result
+"""
+module scalar_multiplication 
+#(
+	parameter ENTRY_SIZE = 5,
+	parameter RESENTRY_SIZE = 9
+)
+(
+	input [ENTRY_SIZE - 1:0] a, b,
+	output [RESENTRY_SIZE - 1:0] result
 );
 
 assign result = a*b;
