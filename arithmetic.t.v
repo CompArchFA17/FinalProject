188a2b00739f878aac952b6f23be3d6ff7bafe3b
arianaolson419 <arianaolson419@gmail.com>
2017-12-06 20:14:18 -0500

    Add test cases

--- arithmetic.t.v
+++ arithmetic.t.v
@@ -1,19 +1,40 @@
+"""Test bench for the scalar multiplication module"""
+
 `include "arithmetic.v"
+module scalar_multiplication_TEST();
 
-module testMult();
+parameter ENTRY_SIZE = 5;
+parameter RESENTRY_SIZE = 9;
 
-reg[4:0] a, b;
-wire[4:0] out;
+reg[ENTRY_SIZE - 1:0] a, b;
+wire[RESENTRY_SIZE - 1:0] out;
 
-multiplication dut (a, b, out);
+scalar_multiplication #(.ENTRY_SIZE(ENTRY_SIZE), .RESENTRY_SIZE(RESENTRY_SIZE)) dut (a, b, out);
 
 initial begin
-a = 4'd4;
-b = 4'd6;
-#10
+	// Multiply by 0
+	a = 5'd0; b = 5'd0; #50
+	if (out !== 9'd0) begin
+		$display("Test failed. Expected output: %b. Actual output: %b", 9'd0, out);
+	end
 
-$display("%d",out);
+	// Multiply by 1
+	a = 5'd1; b = 5'd15; #50
+	if (out !== 9'd15) begin
+		$display("Test failed. Expected output: %b. Actual output: %b", 9'd15, out);
+	end
 
+	// Multiply two numbers that do not overflow the result size
+	a = 5'd25; b = 5'd20; #50
+	if (out !== 9'd500) begin
+		$display("Test failed. Expected output: %b. Actual output: %b", 9'd500, out);
+	end
+
+	// Multiply two numbers that overflow the result size
+	// Expect the most significant bits to be truncated
+	a = 5'd31; b = 5'd25; #50
+	if (out !== 9'd263) begin
+		$display("Test failed. Expected output: %b. Actual output: %b", 9'd263, out);
+	end
 end
-
 endmodule // testMult
