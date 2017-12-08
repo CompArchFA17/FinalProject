551b8cdc6824c69747883e88116d9cb59769dfda
arianaolson419 <arianaolson419@gmail.com>
2017-12-08 12:22:43 -0500

    Add data memory implementation and tests

---
+++ data_memory.v
@@ -0,0 +1,30 @@
+/*
+The memory where matrices are stored.
+*/
+
+module memory
+#(
+    parameter addresswidth  = 32,
+    parameter depth         = addresswidth * 2,
+    parameter width         = 32
+)
+(
+    input 		                clk,
+    output [width-1:0]      dataOut,
+    input [addresswidth-1:0]    address,
+    input                       writeEnable,
+    input [width-1:0]           dataIn
+);
+
+
+    reg [width-1:0] memory [depth-1:0];
+
+    always @(negedge clk) begin
+        if(writeEnable)
+            memory[address >> 2] <= dataIn;
+    end
+    assign dataOut = memory[address >> 2];
+
+    initial $readmemh("matrix_mem.dat", memory);
+
+endmodule
