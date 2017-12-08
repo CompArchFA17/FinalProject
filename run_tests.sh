cb61d00f4312c8e6b37d5df84ee1b165a10b05ad
arianaolson419 <arianaolson419@gmail.com>
2017-12-08 12:23:53 -0500

    Update to include data memory

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,9 @@ make
 
 echo "Running all module tests:"
 
+cat mem_test_data.dat > matrix_mem.dat
+echo "Running data memory tests..."
+./data_mem
 echo "Running arithmetic tests..."
 ./arithmetic
 echo "Running dot tests..."
