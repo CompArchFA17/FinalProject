afa73a83a60ddb210659f651aad249ec9602fe37
arianaolson419 <arianaolson419@gmail.com>
2017-12-09 21:34:57 -0500

    Modify to take 9 data entries at a time

--- data_memory.t.v
+++ data_memory.t.v
@@ -6,14 +6,27 @@ Test bench for data memory module.
 
 module memory_TEST();
 	parameter ENTRY_SIZE = 5;
+	parameter ADDRESS_WIDTH = 32;
 	reg clk;
-	reg [31:0] address;
+	reg [ADDRESS_WIDTH - 1:0] addr0, addr1, addr2, addr3, addr4;
+	reg [ADDRESS_WIDTH - 1:0] addr5, addr6, addr7, addr8;
 	reg writeEnable;
-	reg [ENTRY_SIZE - 1:0] dataIn;
+	reg [ENTRY_SIZE - 1:0] dataIn0, dataIn1, dataIn2, dataIn3, dataIn4;
+	reg [ENTRY_SIZE - 1:0] dataIn5, dataIn6, dataIn7, dataIn8;
 
-	wire [ENTRY_SIZE - 1:0] dataOut;
+	wire [ENTRY_SIZE - 1:0] data0, data1, data2, data3, data4;
+	wire [ENTRY_SIZE - 1:0] data5, data6, data7, data8;
 
-	memory #(.width(ENTRY_SIZE) dut(clk, dataOut, address, writeEnable, dataIn);
+	memory #(.width(ENTRY_SIZE), .addresswidth(ADDRESS_WIDTH)) dut(
+		.clk(clk), .data0(data0), .data1(data1), .data2(data2),
+		.data3(data3), .data4(data4), .data5(data5), .data6(data6),
+		.data7(data7), .data8(data8), .addr0(addr0), .addr1(addr1),
+		.addr2(addr2), .addr3(addr3), .addr4(addr4), .addr5(addr5),
+		.addr6(addr6), .addr7(addr7), .addr8(addr8), .writeEnable(writeEnable),
+		.dataIn0(dataIn0), .dataIn1(dataIn1), .dataIn2(dataIn2),
+		.dataIn3(dataIn3), .dataIn4(dataIn4), .dataIn5(dataIn5),
+		.dataIn6(dataIn6), .dataIn7(dataIn7), .dataIn8(dataIn8)
+	);
 
 	initial clk = 0;
 	always #10 clk = !clk;
@@ -21,33 +34,84 @@ module memory_TEST();
 		$dumpfile("memory.vcd");
 		$dumpvars(0, memory_TEST, dut.memory[0]);
 
+		dataIn0 = 5'b00000; dataIn1 = dataIn0; dataIn2 = dataIn0;
+		dataIn3 = dataIn0; dataIn4 = dataIn5; dataIn6 = dataIn0;
+		dataIn7 = dataIn0; dataIn8 = dataIn0;
+
+		addr0 = 32'd0;
+		addr1 = addr0 + ADDRESS_WIDTH;
+		addr2 = addr1 + ADDRESS_WIDTH;
+		addr3 = addr2 + ADDRESS_WIDTH;
+		addr4 = addr3 + ADDRESS_WIDTH;
+		addr5 = addr4 + ADDRESS_WIDTH;
+		addr6 = addr5 + ADDRESS_WIDTH;
+		addr7 = addr6 + ADDRESS_WIDTH;
+		addr8 = addr7 + ADDRESS_WIDTH;
 		// Test Case 1: Do not write if writeEnable is low.
-		writeEnable = 0; dataIn = 5'hffff; address = 5'd0;
+		writeEnable = 0; dataIn0 = 5'h1f;
 		#20
-		if (dataOut === dataIn) begin
+		if (data0 === dataIn0) begin
 			$display("Test case 1 failed: memory was written to when writeEnable was false.");
 		end
 
-		else if (dataOut === 5'bx) begin
+		else if (data0 === 5'bx) begin
 			$display("Test case 1 failed: there is no memory at the given address.");
 		end
 
 		#1000
 		// Test case 2: Write to memory if writeEnable is high.
-		writeEnable = 1; dataIn = 5'hffff; address = 5'd0;
+		writeEnable = 1;
+		dataIn0 = 5'h1f;
+		dataIn1 = 5'h1e;
+		dataIn2 = 5'h1d;
+		dataIn3 = 5'h1c;
+		dataIn4 = 5'h1b;
+		dataIn5 = 5'h1a;
+		dataIn6 = 5'h19;
+		dataIn7 = 5'h18;
+		dataIn8 = 5'h17;
+
 		#40
 		
-		if (dataOut === 5'bx) begin
+		if (data0 === 5'bx) begin
 			$display("Test case 2 failed: there is no memory at the given address.");
 		end
 
-		if (dut.memory[0] !== dataIn) begin
+		if (dut.memory[0] !== dataIn0) begin
 			$display("Test case 2 failed: the memory contained at the given address does not match dataIn");
 		end
 
-		if (dataOut !== dataIn) begin
-			$display("Test case 2 failed: memory was not written to when writeEnable was true.");
+		// if (data0 !== dataIn0) begin
+		// 	$display("Test case 2 failed: memory was not written to when writeEnable was true.");
+		// end
+		if (dut.memory[0] !== dataIn0) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn0");
 		end
+		if (dut.memory[1] !== dataIn1) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn1");
+		end
+		if (dut.memory[2] !== dataIn2) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn2");
+		end
+		if (dut.memory[3] !== dataIn3) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn3");
+		end
+		if (dut.memory[4] !== dataIn4) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn4");
+		end
+		if (dut.memory[5] !== dataIn5) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn5");
+		end
+		if (dut.memory[6] !== dataIn6) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn6");
+		end
+		if (dut.memory[7] !== dataIn7) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn7");
+		end
+		if (dut.memory[8] !== dataIn8) begin
+			$display("Test case 2 failed: the memory contained at the given address does not match dataIn8");
+		end
+
 
 		#1000
 
--- data_memory.v
+++ data_memory.v
@@ -11,10 +11,13 @@ module memory
 )
 (
     input 		                clk,
-    output [width-1:0]      dataOut,
-    input [addresswidth-1:0]    address,
+    output [width-1:0]      data0, data1, data2, data3, data4,
+    output [width-1:0]      data5, data6, data7, data8,
+    input [addresswidth-1:0]    addr0, addr1, addr2, addr3, addr4,
+    input [addresswidth-1:0]    addr5, addr6, addr7, addr8,
     input                       writeEnable,
-    input [width-1:0]           dataIn
+    input [width-1:0]           dataIn0, dataIn1, dataIn2, dataIn3, dataIn4,
+    input [width-1:0]           dataIn5, dataIn6, dataIn7, dataIn8
 );
 
 
@@ -22,9 +25,26 @@ module memory
 
     always @(negedge clk) begin
         if(writeEnable)
-            memory[address >> 2] <= dataIn;
+            memory[addr0 >> 2] <= dataIn0;
+            memory[addr1 >> 2] <= dataIn1;
+            memory[addr2 >> 2] <= dataIn2;
+            memory[addr3 >> 2] <= dataIn3;
+            memory[addr4 >> 2] <= dataIn4;
+            memory[addr5 >> 2] <= dataIn5;
+            memory[addr6 >> 2] <= dataIn6;
+            memory[addr7 >> 2] <= dataIn7;
+            memory[addr8 >> 2] <= dataIn8;
     end
-    assign dataOut = memory[address >> 2];
+
+    assign data0 = memory[addr0 >> 2];
+    assign data1 = memory[addr1 >> 2];
+    assign data2 = memory[addr2 >> 2];
+    assign data3 = memory[addr3 >> 2];
+    assign data4 = memory[addr4 >> 2];
+    assign data5 = memory[addr5 >> 2];
+    assign data6 = memory[addr6 >> 2];
+    assign data7 = memory[addr7 >> 2];
+    assign data8 = memory[addr8 >> 2];
 
     initial $readmemh("matrix_mem.dat", memory);
 
