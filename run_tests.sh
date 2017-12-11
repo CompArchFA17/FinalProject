4fed8984c89ca5a14182c2b118da8471934ab0d3
arianaolson419 <arianaolson419@gmail.com>
2017-12-11 16:31:18 -0500

    Add tests

--- run_tests.sh
+++ run_tests.sh
@@ -12,12 +12,22 @@ echo "Running arithmetic tests..."
 echo "Running dot tests..."
 ./dot
 echo "Running matmul tests..."
-./matmul
+./matrixmultiplication
 echo "Running load block tests..."
 ./load_block
+echo "Running add block tests..."
+./add_block
+echo "Running registers tests..."
+./registers
+echo "Running multiplier tests..."
+./multiplier
+echo "Running multiplexer tests..."
+./multiplexer
 echo "running fsm tests..."
 ./fsm
 echo "running program memory tests..."
 ./prog_mem
 echo "running controller tests..."
 ./controller
+echo "Running multiplier_network tests..."
+./multiplier_network
