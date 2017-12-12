4f48db1055f8653670cb1c82a578d39e0561e266
Ariana Olson <arianaolson419@gmail.com>
2017-12-12 01:53:39 -0500

    Remove unnecessary lines

--- multiplier6by6.t.v
+++ multiplier6by6.t.v
@@ -7,22 +7,9 @@ Test bench for the full 6 by 6 multiplier
 module multiplier6by6_TEST();
 	`define CLK_DELAY #50
 
-	`define EXPECTED_AEBG 45'b101011100010101100111101111001100011001110010
-	`define EXPECTED_AFBH 45'b100101000101111101001001001110100100111001100
-	`define EXPECTED_CEDG 45'b001110111101110100111001011000101111101010110
-	`define EXPECTED_CFDH 45'b011000110000111101011000101111101111011010010
-	parameter NUM_TESTS = 1;
-
 	reg clk;
 
-	reg[5:0] success_count;
-
 	multiplier6by6 dut (.clk(clk));
-
-	`define AEplusBG dut.network.chooseres.AEplusBG
-	`define AFplusBH dut.network.chooseres.AFplusBH
-	`define CEplusBG dut.network.chooseres.CEplusDG
-	`define CFplusDH dut.network.chooseres.CFplusDH
 
 	initial clk = 0;
 	always `CLK_DELAY clk = !clk;
@@ -33,14 +20,7 @@ module multiplier6by6_TEST();
 		$dumpfile("multiplier6by6.vcd");
 		$dumpvars();
 		
-		#50
 		#5000
-
-		if (dut.network.chooseres.AEplusBG[44:40] === 5'd21) $display("success");
-		$display("AE + BG %b", dut.network.chooseres.AEplusBG);
-		$display("AF + BH %b", dut.network.chooseres.AFplusBH);
-		$display("CE + DG %b", dut.network.chooseres.CEplusDG);
-		$display("CF + DH %b", dut.network.chooseres.CFplusDH);
 
 		mem_out = $fopen("memory_out.txt");
 		for (i=0; i<1024; i=i+1) begin
